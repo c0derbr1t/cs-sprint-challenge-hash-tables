d2363cbec4fc2a033a52a789a4adb524c1c04af2
c0derbr1t <c0der.br1t@gmail.com>
2020-10-02 11:07:46 -0500

    Passed all tests for ex5.py

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,21 +1,34 @@
 # Your code here
-
-
+cache = {}
 
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    for file in files:
+        file_split = file.split("/")
+        if file_split[-1] in cache:
+            cache[file_split[-1]].append(file)
+        else:
+            cache[file_split[-1]] = [file]
 
-    return result
+    results = []
+
+    for query in queries:
+        if query in cache:
+            for item in cache[query]:
+                results.append(item)
+
+    return results
 
 
 if __name__ == "__main__":
     files = [
         '/bin/foo',
         '/bin/bar',
-        '/usr/bin/baz'
+        '/usr/bin/baz',
+        '/usr/bar/foo'
     ]
     queries = [
         "foo",
