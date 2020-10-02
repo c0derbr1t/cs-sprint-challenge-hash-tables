1cea87996c65f9dbe68673f0bebf8415d76410ed
c0derbr1t <c0der.br1t@gmail.com>
2020-10-02 14:24:44 -0500

    Attempt but did not finish ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,17 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    print(weights)
+    table = {item: i for (i, item) in enumerate(weights)}
 
-    return None
+    current = limit - weight
+    weight_b = None
+
+    # UNSURE OF THE REST
+
+    print(table)
+
+    
+
+
+    # return None
