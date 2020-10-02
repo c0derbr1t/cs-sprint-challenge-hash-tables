4574ae1adda90315187eb79fd81c4329121a22ef
c0derbr1t <c0der.br1t@gmail.com>
2020-10-02 11:38:15 -0500

    Passed all tests for ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,20 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    numbers = {}
+    sort_list = sorted(a, reverse=True)
+
+    for item in sort_list:
+        if item >= 0 and item not in numbers:
+            numbers[item] = 0
+        if item < 0 and -item in numbers:
+            numbers[-item] += 1
+
+    # print(numbers)
+    result = []
+    for number in numbers:
+        if numbers[number] > 0:
+            result.append(number)
 
     return result
 
